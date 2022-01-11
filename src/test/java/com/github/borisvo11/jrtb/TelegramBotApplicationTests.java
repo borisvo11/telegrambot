b632d82d5f8f62c748bc9e146c81bbb4765046da
Baruch <brsv11@gmail.com>
2022-01-11 16:05:18 +0200

    After Spring Boot

---
+++ src/test/java/com/github/borisvo11/jrtb/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.borisvo11.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
